7c88391780eba8f3137bc0c368c5c4b441cb60e1
secforgeek <secforgeek@gmail.com>
2018-01-02 01:29:35 +0000

    Database Backup

--- db_backup/restro.sql
+++ db_backup/restro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2017 at 06:19 PM
+-- Generation Time: Jan 02, 2018 at 02:29 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -34,6 +34,17 @@ CREATE TABLE `client_shop_category` (
   `category_id` varchar(32) NOT NULL,
   `category` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `client_shop_category`
+--
+
+INSERT INTO `client_shop_category` (`id`, `client_id`, `category_id`, `category`) VALUES
+(1, '5d41402abc4b2a76b9719d688917c592', 'cc03e747a6afbbcbf8be7668acfebee5', 'Drinks'),
+(2, '5d41402abc4b2a76b9719d688917c592', '16d7a4fca7442dda3ad93c9a726597e4', 'Main Course'),
+(3, '5d41402abc4b2a76b9719d911017c592', 'c06db68e819be6ec3d26c6038d8e8d1f', 'Drinks'),
+(4, '5d41402abc4b2a76b9719d911017c592', 'd12db68e819be6ec3d26c6038d8e8d1f', 'Main Course'),
+(6, '5d41402abc4b2a76b9719d911017c592', 'f50db68e819be6ec3d26c6038d8e8d1f', 'Dessert');
 
 -- --------------------------------------------------------
 
@@ -73,11 +84,27 @@ CREATE TABLE `client_shop_menu` (
   `id` int(11) NOT NULL,
   `client_id` varchar(32) NOT NULL,
   `category_id` varchar(32) NOT NULL,
-  `category` varchar(32) NOT NULL,
+  `item_topic_id` varchar(32) NOT NULL,
   `item_topic` varchar(32) NOT NULL,
-  `item_desc` varchar(200) NOT NULL,
+  `item_desc_yn` char(1) NOT NULL DEFAULT 'N',
+  `item_desc` varchar(200) DEFAULT NULL,
   `price` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `client_shop_menu`
+--
+
+INSERT INTO `client_shop_menu` (`id`, `client_id`, `category_id`, `item_topic_id`, `item_topic`, `item_desc_yn`, `item_desc`, `price`) VALUES
+(1, '5d41402abc4b2a76b9719d688917c592', 'cc03e747a6afbbcbf8be7668acfebee5', '4ff772b41b4067f9c2d047588232a09e', 'Pepsi', 'N', 'Adivinar ensuenos circulos maltrato don doy albergue siguiera. Estetico seriedad tertulia las conviene uso esa tal. No tu pasara consta recibo un activo oh camara. Ya so plazos un afuera esposa gloria', 2.99),
+(2, '5d41402abc4b2a76b9719d688917c592', 'cc03e747a6afbbcbf8be7668acfebee5', '8760a746182f419056a7e062ddef25c3', '7-Up', 'N', 'Adivinar ensuenos circulos maltrato don doy albergue siguiera. Estetico seriedad tertulia las conviene uso esa tal. No tu pasara consta recibo un activo oh camara', 1.99),
+(3, '5d41402abc4b2a76b9719d688917c592', 'cc03e747a6afbbcbf8be7668acfebee5', '5f2ad42c2ac4a122bdf0af577def57aa', 'Fanta', 'N', 'Adivinar ensuenos circulos maltrato don doy albergue siguiera. Estetico seriedad tertulia las conviene uso esa tal. No tu pasara consta recibo un activo oh camara', 3.99),
+(4, '5d41402abc4b2a76b9719d688917c592', '16d7a4fca7442dda3ad93c9a726597e4', 'bbe2b04aefadfdaef22872ba943fabc0', 'Chicken Pizza', 'Y', 'Adivinar ensuenos circulos maltrato don doy albergue siguiera. Estetico seriedad tertulia las conviene uso esa tal. No tu pasara consta recibo un activo oh camara', 10.99),
+(5, '5d41402abc4b2a76b9719d688917c592', '16d7a4fca7442dda3ad93c9a726597e4', '906e488872e682abff953a955c3b95b6', 'Lamp Pizza', 'Y', 'Adivinar ensuenos circulos maltrato don doy albergue siguiera. Estetico seriedad tertulia las conviene uso esa tal. No tu pasara consta recibo un activo oh camara', 12.99),
+(6, '5d41402abc4b2a76b9719d911017c592', 'c06db68e819be6ec3d26c6038d8e8d1f', '4334dc6379c8f95ddf11b8508cfea271', 'Pepsi', 'N', 'Adivinar ensuenos circulos maltrato don doy albergue siguiera. Estetico seriedad tertulia las conviene uso esa tal. No tu pasara consta recibo un activo oh camara', 2.99),
+(7, '5d41402abc4b2a76b9719d911017c592', 'c06db68e819be6ec3d26c6038d8e8d1f', 'f107ca7fd817fda1b59e92ae29959cc4', 'Sprite', 'N', 'Adivinar ensuenos circulos maltrato don doy albergue siguiera. Estetico seriedad tertulia las conviene uso esa tal. No tu pasara consta recibo un activo oh camara', 1.99),
+(8, '5d41402abc4b2a76b9719d911017c592', 'd12db68e819be6ec3d26c6038d8e8d1f', '75a5ffa8b63ae26a6955e3a1c730b028', 'Biryani', 'Y', 'Adivinar ensuenos circulos maltrato don doy albergue siguiera. Estetico seriedad tertulia las conviene uso esa tal. No tu pasara consta recibo un activo oh camara', 6.99),
+(9, '5d41402abc4b2a76b9719d911017c592', 'f50db68e819be6ec3d26c6038d8e8d1f', '352eb4c555792ae060fbf9112dde56f7', 'Vanilla ', 'Y', 'Adivinar ensuenos circulos maltrato don doy albergue siguiera. Estetico seriedad tertulia las conviene uso esa tal. No tu pasara consta recibo un activo oh camara', 3.5);
 
 -- --------------------------------------------------------
 
@@ -122,25 +149,26 @@ CREATE TABLE `client_shop_search` (
   `name` varchar(50) NOT NULL,
   `profile_img` varchar(150) NOT NULL,
   `cuisine` text NOT NULL,
-  `delivery_fee` int(3) NOT NULL,
-  `delivery_dis` int(2) NOT NULL,
-  `min_order` int(3) NOT NULL,
+  `delivery_fee` int(3) NOT NULL DEFAULT '0',
+  `delivery_dis` int(2) NOT NULL DEFAULT '0',
+  `min_order` int(3) NOT NULL DEFAULT '0',
   `lat` double NOT NULL,
   `lng` double NOT NULL,
   `category` varchar(50) NOT NULL,
   `sub_category` varchar(50) NOT NULL,
   `table_dir` varchar(50) NOT NULL,
-  `city` varchar(50) NOT NULL
+  `city` varchar(50) NOT NULL,
+  `watznear_charge` int(4) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `client_shop_search`
 --
 
-INSERT INTO `client_shop_search` (`id`, `client_id`, `active`, `name`, `profile_img`, `cuisine`, `delivery_fee`, `delivery_dis`, `min_order`, `lat`, `lng`, `category`, `sub_category`, `table_dir`, `city`) VALUES
-(2, '5d41402abc4b2a76b9719d911017c592', 'Y', 'Pizza Hut', '', '', 1, 2, 8, 54.973286, -2.095061, 'Restaurant', 'Takeaway', 'client_shop_menu', 'Hexham'),
-(3, '202cb962ac59075b964b07152d234b70', 'Y', 'SecondRestro', '', '', 1, 3, 6, 54.971877, -2.100992, 'Restaurant', 'Takeaway', '', ''),
-(4, '5d41402abc4b2a76b9719d688917c592', 'Y', 'Corbridge', '', '', 2, 7, 10, 54.968306, -1.985878, 'Restaurant', 'Takeaway', '', '');
+INSERT INTO `client_shop_search` (`id`, `client_id`, `active`, `name`, `profile_img`, `cuisine`, `delivery_fee`, `delivery_dis`, `min_order`, `lat`, `lng`, `category`, `sub_category`, `table_dir`, `city`, `watznear_charge`) VALUES
+(2, '5d41402abc4b2a76b9719d911017c592', 'Y', 'Pizza Hut', '', 'Indian', 1, 2, 8, 54.973286, -2.095061, 'Restaurant', 'Takeaway', 'client_shop_menu', 'Hexham', 0),
+(3, '202cb962ac59075b964b07152d234b70', 'Y', 'SecondRestro', '', 'Pizza, Italian', 1, 3, 6, 54.971877, -2.100992, 'Restaurant', 'Takeaway', '', '', 0),
+(4, '5d41402abc4b2a76b9719d688917c592', 'Y', 'Corbridge', '', 'Snacks, Indian', 2, 7, 10, 54.968306, -1.985878, 'Restaurant', 'Takeaway', '', '', 10);
 
 -- --------------------------------------------------------
 
@@ -182,7 +210,7 @@ CREATE TABLE `users_login` (
 --
 
 INSERT INTO `users_login` (`id`, `profile_status`, `joined`, `type`, `name`, `email`, `password`, `last_changed`, `token`, `mobile`, `verified_mob`, `verified_email`, `updated_at`) VALUES
-(1, 'N', '2017-12-05 20:46:22', 'U', 'Test', 'username@gmail.com', '$2y$10$L3LTdPQAlaEtCh2V8EcwnO/uv2rZ1AMmZ3iaR4svPzuUL5vC2TRSC', NULL, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE1MTI5NDA1MTMsImV4cCI6MzAyNzA5MDYyNiwiaXNzIjoiZ3Nkcm9pZC5jb20iLCJkYXRhIjp7InVzciI6IlRlc3QiLCJ0eXBlIjoiVSIsImVtYWlsIjoidXNlcm5hbWVAZ21haWwuY29tIn19.Jkyo1n0I6iaj7VEzs5Ia71cvg848fCQNeMddklKi3kYxD0r2RdzSre30NgrMo0kgJfg5w1rD802rG-gwTN_ZKVijcbHAjP01I5B4nzlyv_VsO9n2ERI3pR1xKeyd9mJkiZxhpy6jpB7SIAQ7tM-ZbvMA6PKGEtQBKDMQipl5X7k', 1234567890, '', '', '2017-12-10 21:15:13'),
+(1, 'N', '2017-12-05 20:46:22', 'U', 'Test', 'username@gmail.com', '$2y$10$L3LTdPQAlaEtCh2V8EcwnO/uv2rZ1AMmZ3iaR4svPzuUL5vC2TRSC', NULL, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE1MTQ2NzE2ODUsImV4cCI6MzAzMDU1Mjk3MCwiaXNzIjoiZ3Nkcm9pZC5jb20iLCJkYXRhIjp7InVzciI6IlRlc3QiLCJ0eXBlIjoiVSIsImVtYWlsIjoidXNlcm5hbWVAZ21haWwuY29tIn19.HePE7hlK4H2JjEzh_juCNk6q-FaxR7Bi4FtbDQXFcDr7eKxlq3qmn-0BvIJDDTv1fxu0IWdEfVGhjUZx2LjkN4j5oQVJYUHBrx3zs4Q5LTeb3ZRmOoZ_0fpnclSC6iA7liW0yWqcuV-dyjbWqHk6m4NKJHFc3SMWiPeOw1sjD2U', 1234567890, '', '', '2017-12-30 22:08:05'),
 (2, 'N', '2017-12-05 21:50:43', 'U', 'Test2', 'usernameh@gmail.com', '$2y$10$OflNGOLMHxOFn5V5gUSsgeGAIAvhUwE1m1eVqNlsHVaIILWhxfBEi', '2017-12-05 21:50:43', 'eyJ0eXAiOiJKV1QiLCJhbGcdsadahhsddsadaaiOiJSUzI1NiJ9.eyJpc3MiOiJnc2Ryb2lkLmNvbSIsImF1ZCI6ImdzZHJvaWQuY29tIiwidXNyIjoidXNlcm5hbWVoQGdtYWlsLmNvbSIsInR5cGUiOiJVIn0.WXhF41PvgUUAo2k_217gRsLrw2AzQJLzPg-HX7FmO_G756-TJhmr5MBgHOMS-71MaLNmXoK-6mv1_SF3H1M8eCOPVACLih20bnfeJohQJhU1kJeOmBcErxZkVaulkA2Yvfj2DtIgXIxC', 1234567895, '', '', '2017-12-10 20:43:02'),
 (3, 'N', '2017-12-05 23:42:22', 'U', 'asdsadghf', 'usernam@gmail.com', '$2y$10$bPJGMIxgzhhtxFDxtOsY9.K72GoAfB1LaxPoZbUdYH6ZM2gU.cURG', '2017-12-05 23:42:22', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJnc2Ryb2lkLmNvbSIsImF1ZCI6ImdzZHJvaWQuY29tIiwidXNyIjoidXNlcm5hbUBnbWFpbC5jb20iLCJ0eXBlIjoiVSJ9.aOjaQo5fzhBFGexqmNzudyIePa1Nd7nqSADNc0JxnISfQfUzkGkjgjeVSP-tIOEkli1EtPasDncimtaSseGDxW4vYfcEcDKYsNzmgbNiVBfcynvV0lifWXccYDErt9T2XQMMU_sNi1CPprh5hzWhtp6qBzRCg3', 1234567894, '', '', '2017-12-05 23:42:22'),
 (4, 'N', '2017-12-05 23:44:07', 'U', 'Akshay', 'akshay@gmail.com', '$2y$10$dO3IN05IIink0miMmIAf2u5pintq.OPcad/WtOucYmTRl5ITPhwY2', '2017-12-05 23:44:07', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJnc2Ryb2lkLmNvbSIsImF1ZCI6ImdzZHJvaWQuY29tIiwidXNyIjoiYWtzaGF5QGdtYWlsLmNvbSIsInR5cGUiOiJVIn0.t99FBl5sfGOm3HvAhpm2vNXgiBxYJ9Ts58GTf8pRoaqy9nZjAa9nYxUTs0B1L2Fl6ykHvGEGkdbOtfrsEuZaTGkTe3MvQzu4gGbW1VWSKhTybtXXPnuGK6fjSVQuKsDfw7WayNifEiccdhr-zmCtd_0lRyhLmjF', 1234567891, '', '', '2017-12-05 23:44:07'),
@@ -224,8 +252,9 @@ INSERT INTO `user_profile` (`id`, `email`, `firstname`, `lastname`, `mobile`, `a
 --
 ALTER TABLE `client_shop_category`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `category_id` (`category_id`) USING BTREE,
   ADD KEY `client_id` (`client_id`),
-  ADD KEY `category_id` (`category_id`);
+  ADD KEY `category` (`category`);
 
 --
 -- Indexes for table `client_shop_details`
@@ -240,6 +269,7 @@ ALTER TABLE `client_shop_details`
 --
 ALTER TABLE `client_shop_menu`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `item_topic_id` (`item_topic_id`),
   ADD KEY `client_id` (`client_id`),
   ADD KEY `category_id` (`category_id`);
 
@@ -290,7 +320,7 @@ ALTER TABLE `user_profile`
 -- AUTO_INCREMENT for table `client_shop_category`
 --
 ALTER TABLE `client_shop_category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `client_shop_details`
@@ -302,7 +332,7 @@ ALTER TABLE `client_shop_details`
 -- AUTO_INCREMENT for table `client_shop_menu`
 --
 ALTER TABLE `client_shop_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `client_shop_owners`
